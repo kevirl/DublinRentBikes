689820df35633584e4ba07789046738959deae90
Kevin <kevirl@gmail.com>
2016-08-16 14:35:57 +0000

    added static pages for Home About and Contact Us

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,10 @@
 Rails.application.routes.draw do
+  get 'static_pages/home'
+
+  get 'static_pages/about'
+
+  get 'static_pages/contact_us'
+
   resources :rental_items
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
